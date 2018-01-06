9bc6d63d6f061c4ede0ff7af81b31500b72ea184
Seunghoon Baek <seung51hoon@gmail.com>
2018-01-07 02:14:52 +0900

    Update test code

--- SeqBoardCreator.py
+++ SeqBoardCreator.py
@@ -28,7 +28,7 @@ class SeqBoardCreator:
         circleImage = self._createResizedImage(circleImagePath, circleWidthPixel, circleHeightPixel)
         cv2.putText(circleImage,
                     "{}".format(number),
-                    (circleWidthPixel * 0.5, circleHeightPixel * 0.5),
+                    (int(circleWidthPixel * 0.5), int(circleHeightPixel * 0.5)),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1.0,
                     (255, 255, 255), 3)
@@ -39,7 +39,7 @@ class SeqBoardCreator:
         return self._createResizedImage(self.params.backgroundImagePath, self.params.widthPixel, self.params.heightPixel)
 
     def _createResizedImage(self, targetImagePath, targetWidthPixel, targetHeightPixel):
-        print("_createResizedImage")
+        print("_createResizedImage, targetImagePath : [{}]".format(targetImagePath))
         img = cv2.imread(targetImagePath, cv2.IMREAD_UNCHANGED)
 
         if img.shape[2] != 4 : ## get Image channel
@@ -48,8 +48,13 @@ class SeqBoardCreator:
         return cv2.resize(img, (targetWidthPixel, targetHeightPixel))
 
     def _overlayImage(self, baseImage, overlayImage, offsetX, offsetY):
-        print("_overlayImage" + str(offsetX) + ", " + str(offsetY))
+        print("_overlayImage, overlayImage.shape[0] : [{}], overlayImage.shape[1] : [{}], overlayImage.shape[2] : [{}]".format(overlayImage.shape[0], overlayImage.shape[1], overlayImage.shape[2]))
+        print(
+        "_overlayImage, baseImage.shape[0] : [{}], baseImage.shape[1] : [{}], baseImage.shape[2] : [{}]".format(
+            baseImage.shape[0], baseImage.shape[1], baseImage.shape[2]))
+
         baseImage[offsetY:offsetY + overlayImage.shape[0], offsetX:offsetX + overlayImage.shape[1]] = overlayImage
+        ## baseImage = self.overlay_transparent(baseImage, overlayImage, offsetX, offsetY)
 
     def _writeImageFiles(self, outputImages):
         print("_writeImageFiles")
@@ -75,6 +80,7 @@ class SeqBoardCreator:
         while indexOfPath < len(fillImagePaths) :
 
             baseImage = self._createBackgroundImage()
+            print "[DEBUG-POINT] baseImage : [{}]".format(self.params.backgroundImagePath)
             for row in xrange(self.params.numOfRows):
                 for col in xrange(self.params.numOfCols):
                     offsetX = int((self.params.heightPixel / self.params.numOfCols) * col)
@@ -83,6 +89,7 @@ class SeqBoardCreator:
                     indexOfPath += 1
 
                     resizedImage = self._createResizedImage(notPickedImagePath, thumbnailWidth, thumbnailHeight)
+                    print "[DEBUG-POINT] _createResizedImage : [{}]".format(notPickedImagePath)
                     self._overlayImage(baseImage,
                                        resizedImage,
                                        offsetX + self.paddingLeftPixel,
@@ -95,7 +102,75 @@ class SeqBoardCreator:
                                                                    circleNum)
                     self._overlayImage(baseImage,
                                        circleImage,
-                                       offsetX + (self.params.widthPixel / self.params.numOfCol) - self.circleWidthPixel,
+                                       offsetX + (self.params.widthPixel / self.params.numOfCols) - self.circleWidthPixel,
                                        offsetY + (self.params.heightPixel / self.params.numOfRows) - self.circleHeightPixel)
+
+                    print (self.params.outputDirPath + "/" + str(indexOfPath) + ".jpg")
+                    cv2.imwrite(self.params.outputDirPath + "/" + str(indexOfPath) + ".jpg", baseImage)
             overlayImages.append(baseImage)
         return overlayImages
+
+    def OverlayImage(self, src, overlay, posx, posy):
+        S = (0.5, 0.5, 0.5, 0.5)  # Define blending coefficients S and D
+        D = (0.5, 0.5, 0.5, 0.5)
+
+        for x in range(overlay.shape[1]):
+
+            if x + posx < src.shape[1]:
+
+                for y in range(overlay.shape[0]):
+
+                    if y + posy < src.shape[1]:
+
+                        source = cv2.Get2D(src, y + posy, x + posx)
+                        over = cv2.Get2D(overlay, y, x)
+                        merger = [0, 0, 0, 0]
+
+                        for i in range(3):
+                            if over[i] == 0:
+                                merger[i] = source[i]
+                            else:
+                                merger[i] = (S[i] * source[i] + D[i] * over[i])
+
+                        merged = tuple(merger)
+
+                        cv2.Set2D(src, y + posy, x + posx, merged)
+
+    def overlay_transparent(self, background_img, img_to_overlay_t, x, y, overlay_size=None):
+        """
+        @brief      Overlays a transparant PNG onto another image using CV2
+
+        @param      background_img    The background image
+        @param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
+        @param      x                 x location to place the top-left corner of our overlay
+        @param      y                 y location to place the top-left corner of our overlay
+        @param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
+
+        @return     Background image with overlay on top
+        """
+
+        bg_img = background_img.copy()
+
+        if overlay_size is not None:
+            img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+
+        # Extract the alpha mask of the RGBA image, convert to RGB
+        b, g, r, a = cv2.split(img_to_overlay_t)
+        overlay_color = cv2.merge((b, g, r, a))
+
+        # Apply some simple filtering to remove edge noise
+        mask = cv2.medianBlur(a, 5)
+
+        h, w, _ = overlay_color.shape
+        roi = bg_img[y:y + h, x:x + w]
+
+        # Black-out the area behind the logo in our original ROI
+        img1_bg = cv2.bitwise_and(roi.copy(), roi.copy(), mask=cv2.bitwise_not(mask))
+
+        # Mask out the logo from the logo image.
+        img2_fg = cv2.bitwise_and(overlay_color, overlay_color, mask=mask)
+
+        # Update the original image with our new ROI
+        bg_img[y:y + h, x:x + w] = cv2.add(img1_bg, img2_fg)
+
+        return bg_img
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def getSeqBoardParams() :
 
     params.pickedImagePaths = [
         "images/1.jpg",
-        "images/2.jpimagesgiamges/",
+        "images/2.jpg",
         "images/3.jpg",
         "images/4.jpg",
         "images/5.jpg",
